a327a93176f8b02ee9d3e06eb0902568fba43d53
mschu <chuminsung@gmail.com>
2020-03-26 22:40:08 +0900

    saving_user_name

---
+++ nomad-vanilla-js/greeting.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const newUser = input.value;
+    paintGreeting(newUser);
+    saveName(newUser);
+}
+
+function askForName() {
+    greeting.classList.remove(SHOWING_CN);
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}! This is momentum.`;
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        askForName();
+    }
+    else {
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
